718392f65d4729f893bf273c62e223dc5002c856
PoProstuWitold <witekzawada@o2.pl>
2022-12-29 20:50:14 +0100

    guilds & threads events

---
+++ src/events/guilds.ts
@@ -0,0 +1,29 @@
+import type { ArgsOf } from 'discordx'
+import { Discord, On } from 'discordx'
+import { injectable } from 'tsyringe'
+
+import { CustomLogger } from '../services/logger.js'
+
+
+@Discord()
+@injectable()
+export class Threads {
+
+	constructor(
+        private logger: CustomLogger
+    ) {}
+
+	@On({
+		event: 'guildCreate'
+	})
+	guildCreate([guild]: ArgsOf<'guildCreate'>): void {
+		this.logger.info(`Joined guild - id: "${guild.id}", name: "${guild.name}"`)
+	}
+
+    @On({
+		event: 'guildDelete'
+	})
+	guildDelete([guild]: ArgsOf<'guildDelete'>): void {
+		this.logger.info(`Left guild - id: "${guild.id}", name: "${guild.name}"`)
+	}
+}
---
+++ src/events/threads.ts
@@ -0,0 +1,34 @@
+import type { ArgsOf } from 'discordx'
+import { Discord, On } from 'discordx'
+import { injectable } from 'tsyringe'
+
+import { CustomLogger } from '../services/logger.js'
+
+
+@Discord()
+@injectable()
+export class Threads {
+
+	constructor(
+        private logger: CustomLogger
+    ) {}
+
+	@On({
+		event: 'threadCreate'
+	})
+	async threadCreate([thread]: ArgsOf<'threadCreate'>): Promise<void> {
+		try {
+            if(thread.joinable && !thread.joined) {
+                this.logger.warn(`Unable to join created thread - name: "${thread.name}", id: "${thread.guildId}"`)
+                return
+            }
+            await thread.join()
+            if(thread.joined) {
+                this.logger.info(`Joined thread: "${thread.name}"`, thread.guildId)
+            }
+        } catch (err) {
+            this.logger.error(err)
+            return
+        }
+	}
+}
